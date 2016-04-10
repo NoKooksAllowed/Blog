d219f8f35b8bcb2833932de684e9f70f2e99f232
NoKooksAllowed <taylorpagechicago@gmail.com>
2016-04-10 17:10:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   
   $('.contactLink').click(function(e) {
     $('.test').hide(1000);
-    $('.test').show(2000).load('contact.html');
+    $('.test').show(1000).load('contact.html');
     e.stopImmediatePropagation();
     e.preventDefault();
   });
