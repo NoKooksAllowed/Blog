43a8ce91e0ff0b508bc97d1a86f3c231571048d8
NoKooksAllowed <taylorpagechicago@gmail.com>
2016-04-12 14:23:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
   $('.test').load('home.html');
   
-  $(function() {
-    $( "#accordion" ).accordion();
-  });
-  
   $('#homeLink').click(function(){
     $('.test').hide(100);
     $('.test').show(100).load('home.html');
@@ -25,4 +21,31 @@ $(document).ready(function(){
   }, function() {
     $(this).css('background-color', 'rgb(100,100,255)')
   });
+  
+  //accordion
+  
+  function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+ 
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+ 
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+ 
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+ 
+        e.preventDefault();
+    });
+});
+  
 });
